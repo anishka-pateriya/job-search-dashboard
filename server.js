8f7a17111054b93e3a72483ad6e260b48ea8769c
anishka-pateriya <apateriy@purdue.edu>
2026-06-22 20:13:40 -0700

    fix static file serving

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
